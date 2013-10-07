f51a5f6eb991f71e0507cb4588eae212fda95727
Nikita Nikitin <berozzy@gmail.com>
2013-10-07 21:39:45 +0300

    Code refactoring

--- app.rb
+++ app.rb
@@ -17,9 +17,9 @@ configure do
   set :styles, "styles/"
   set :scripts, "scripts/"
   set :posts, "posts/"
+  set :helpers, "helpers/"
   set :per_page, 5
   set :show_full_post, true
-  set :helpers, "/helpers/"
 
   Slim::Engine.set_default_options pretty: (settings.environment == :development ? true : false), sort_attrs: true
 
@@ -35,10 +35,7 @@ configure do
   set :sass, Compass.sass_engine_options
 end
 
-
-#Dir[settings.helpers+"*.rb"].each {|file| require file if file.exists? }
-
-require_relative 'helpers/routes'
+Dir["#{settings.helpers}*.rb"].each {|file| require_relative settings.helpers + File.basename(file, ".rb")}
 
 class Blog < Sinatra::Application
   include Methods
@@ -58,14 +55,14 @@ class Blog < Sinatra::Application
   end
 
   get '/*.css' do |file|
-    if File.exists? "#{settings.views}#{settings.styles}#{file}.sass"
+    if File.exists? "#{settings.views+settings.styles+file}.sass"
       content_type 'text/css', :charset => 'utf-8'
       sass :"#{settings.styles}/#{file}"
     else redirect '/' end
   end
 
   get '/*.js' do |file|
-    if File.exists? "#{settings.views}#{settings.scripts}#{file}.coffee"
+    if File.exists? "#{settings.views+settings.scripts+file}.coffee"
       content_type 'text/javascript', :charset => 'utf-8'
       coffee :"#{settings.scripts}/#{file}"
     else redirect '/' end
