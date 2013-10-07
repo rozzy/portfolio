ac5686ea7d6d80b45ba24796b572b7a0ec288c13
Nikita Nikitin <berozzy@gmail.com>
2013-10-07 21:28:52 +0300

    Better architecture

--- app.rb
+++ app.rb
@@ -20,8 +20,6 @@ configure do
   set :per_page, 5
   set :show_full_post, true
   set :helpers, "/helpers/"
-  
-  Dir[settings.helpers+"*.rb"].each {|file| require file if file.exists? }
 
   Slim::Engine.set_default_options pretty: (settings.environment == :development ? true : false), sort_attrs: true
 
@@ -37,44 +35,39 @@ configure do
   set :sass, Compass.sass_engine_options
 end
 
+
+#Dir[settings.helpers+"*.rb"].each {|file| require file if file.exists? }
+
+require_relative 'helpers/routes'
+
 class Blog < Sinatra::Application
+  include Methods
 
-  def set_title title = "Blog"
-    @title = title
+  get '/' do
+    @page = 1
+    slim :index, :layout => true
   end
 
-  def get_items # Get all posts
-    Dir.glob settings.posts + "[^@]*.{md,markdown,mdown,txt,html,slim}"
+  get '/', :provides => ['rss', 'atom', 'xml'] do
+    builder :feed
   end
 
-  def show_posts page = 0 # Show posts on page
-    archive = get_items
-    @all_posts = archive.count
-    @pages = (@all_posts/settings.per_page).ceil
-    posts = []
-    for post in archive
-      posts.push parse post
-    end 
-    posts
+  get '/~(\d)' do |page|
+    @page = page
+    slim :index, :layout => true
   end
 
-  def parse post
-    data = Hash.new
-    data[:url] = File.basename(post).strip().sub(" ", "-")
-    data[:time] = File.ctime post
-    file = File.open(post).read().split "---metaend!\n"
-    data[:meta] = YAML.load file[0]
-    case File.extname post
-      when /\.(md, markdown, mdown)/ then data[:content] = Town.parse file[1]
-      when ".slim" then data[:content] = slim file[1]
-      else data[:content] = file[1]
-    end
-    data[:content] = data[:content].sub "\n", '<br />'
-    data
+  get '/*.css' do |file|
+    if File.exists? "#{settings.views}#{settings.styles}#{file}.sass"
+      content_type 'text/css', :charset => 'utf-8'
+      sass :"#{settings.styles}/#{file}"
+    else redirect '/' end
   end
 
-  def show_pages page
-    #@pages
+  get '/*.js' do |file|
+    if File.exists? "#{settings.views}#{settings.scripts}#{file}.coffee"
+      content_type 'text/javascript', :charset => 'utf-8'
+      coffee :"#{settings.scripts}/#{file}"
+    else redirect '/' end
   end
-
 end
