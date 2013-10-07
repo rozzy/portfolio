6cb23d91e970d7722f5132ba4fcec1f19b5c8245
Nikita Nikitin <berozzy@gmail.com>
2013-10-07 16:17:49 +0300

    Working blog prototype!

--- app.rb
+++ app.rb
@@ -36,14 +36,33 @@ class Blog < Sinatra::Application
   end
 
   def getItems # Get all posts
-    Dir.glob settings.posts + "/[^@]*.{md,markdown,mdown,txt,html,slim}"
+    Dir.glob settings.posts + "[^@]*.{md,markdown,mdown,txt,html,slim}"
   end
 
   def showPosts page = 0 # Show posts on page
-    posts = getItems
-    @all_posts = posts.count
+    archive = getItems
+    @all_posts = archive.count
     @pages = (@all_posts/settings.per_page).ceil
+    posts = []
+    for post in archive
+      posts.push parse post
+    end 
+    posts
+  end
 
+  def parse post
+    data = Hash.new
+    data[:url] = File.basename(post).strip().sub(" ", "-")
+    data[:time] = File.ctime post
+    file = File.open(post).read().split "---metaend!\n"
+    data[:meta] = YAML.load file[0]
+    case File.extname post
+      when /\.(md, markdown, mdown)/ then data[:content] = Town.parse file[1]
+      when ".slim" then data[:content] = slim file[1]
+      else data[:content] = file[1]
+    end
+    data[:content] = data[:content].sub "\n", '<br />'
+    data
   end
 
   def showPages page
---
+++ posts/p1 ps.md
@@ -0,0 +1,11 @@
+--- 
+title: "Hello: world!"
+tags: 2.0, ruby, yaml, blog
+array: 
+    big: arrayed!
+---metaend!
+hello world ***drafts***
+===
+ttsatsdfs sjdf sdj f
+===
+wetwetew 
